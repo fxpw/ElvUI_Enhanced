785357731dd3a4f18a381dedbeab2a400c534c11
fxpw <fxpw@outlook.com>
2025-06-16 01:26:36 +0300

    comment Modules\Datatexts\BattlePass.lua

--- Modules/Datatexts/BattlePass.lua
+++ Modules/Datatexts/BattlePass.lua
@@ -1,39 +1,39 @@
-local E, L, V, P, G = unpack(ElvUI)
-local DT = E:GetModule("DataTexts")
-local EE = E:GetModule("ElvUI_Enhanced")
+-- local E, L, V, P, G = unpack(ElvUI)
+-- local DT = E:GetModule("DataTexts")
+-- local EE = E:GetModule("ElvUI_Enhanced")
 
 
 
-local bpFrame = CreateFrame("Frame","ELVUI_DATATEXT_BPFRAME",UIParent)
-bpFrame:SetSize(200,400)
-bpFrame:SetPoint("CENTER",UIParent)
-bpFrame:CreateBackdrop("Transparent")
-bpFrame:Hide()
+-- local bpFrame = CreateFrame("Frame","ELVUI_DATATEXT_BPFRAME",UIParent)
+-- bpFrame:SetSize(200,400)
+-- bpFrame:SetPoint("CENTER",UIParent)
+-- bpFrame:CreateBackdrop("Transparent")
+-- bpFrame:Hide()
 
--- local lastPanel
+-- -- local lastPanel
 
-local function OnEvent(self)
-
-
-	-- lastPanel = self
-end
-
-local function OnEnter(self)
-
-end
+-- local function OnEvent(self)
 
 
+-- 	-- lastPanel = self
+-- end
+
+-- local function OnEnter(self)
+
+-- end
 
 
-local function OnClick(_, btn)
-	if btn == "LeftButton" then
-		if bpFrame:IsShown() then
-			bpFrame:Hide()
-		else
-			bpFrame:Show()
-		end
-	end
-end
 
 
-DT:RegisterDatatext("BattlePass", {"PLAYER_ENTERING_WORLD", "BAG_UPDATE", "UNIT_INVENTORY_CHANGED"}, OnEvent, nil, OnClick, OnEnter, nil, EE:ColorizeSettingName(L["BattlePass"]))
+-- local function OnClick(_, btn)
+-- 	if btn == "LeftButton" then
+-- 		if bpFrame:IsShown() then
+-- 			bpFrame:Hide()
+-- 		else
+-- 			bpFrame:Show()
+-- 		end
+-- 	end
+-- end
+
+
+-- DT:RegisterDatatext("BattlePass", {"PLAYER_ENTERING_WORLD", "BAG_UPDATE", "UNIT_INVENTORY_CHANGED"}, OnEvent, nil, OnClick, OnEnter, nil, EE:ColorizeSettingName(L["BattlePass"]))

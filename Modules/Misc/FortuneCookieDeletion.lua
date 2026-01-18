3c9d49e9439bc5154bae75379b4be9ee80a8b626
Accidev <accidev@yandex.ru>
2026-01-18 19:30:12 +0300

    123123

--- Modules/Misc/FortuneCookieDeletion.lua
+++ Modules/Misc/FortuneCookieDeletion.lua
@@ -7,31 +7,31 @@ local GetItemInfo = GetItemInfo
 local GetContainerItemID = GetContainerItemID
 local GetContainerItemInfo = GetContainerItemInfo
 local GetContainerNumSlots = GetContainerNumSlots
-local GetContainerItemLink = GetContainerItemLink
+
 
 local FORTUNE_COOKIE_NAME = "Предсказание из печенья"
 
 function M:HookVendorGrays()
 	if not E.db.enhanced.general.deleteFortuneCookie then return end
-	
+
 	local originalGetGraysInfo = B.GetGraysInfo
-	
+
 	function B:GetGraysInfo()
 		local count, value = originalGetGraysInfo(self)
-		
-		if not E.db.enhanced.general.deleteFortuneCookie then 
-			return count, value 
+
+		if not E.db.enhanced.general.deleteFortuneCookie then
+			return count, value
 		end
-		
+
 		local itemList = self.SellFrame.Info.itemList
 		
 		for bag = 0, 4 do
 			for slot = 1, GetContainerNumSlots(bag) do
 				local itemID = GetContainerItemID(bag, slot)
-				
+
 				if itemID then
-					local name, link, rarity, _, _, iType, _, _, _, _, itemPrice = GetItemInfo(itemID)
-					
+					local name, link, rarity = GetItemInfo(itemID)
+
 					if name and name == FORTUNE_COOKIE_NAME and rarity and rarity == 1 then
 						local alreadyAdded = false
 						for _, item in ipairs(itemList) do
@@ -40,20 +40,20 @@ function M:HookVendorGrays()
 								break
 							end
 						end
-						
+
 						if not alreadyAdded then
 							local stackCount = select(2, GetContainerItemInfo(bag, slot)) or 1
 							local price = 0
-							
+
 							tinsert(itemList, {bag, slot, link, price, stackCount})
-							
+
 							value = value + (price * stackCount)
 						end
 					end
 				end
 			end
 		end
-		
+
 		return #itemList, value
 	end
 end

46dbb8a33bd208957d137d8e278394ba85f38f2d
Accidev <accidev@yandex.ru>
2026-06-05 23:28:36 +0300

    Fix Имя гильдии под ником.

--- Config/Config.lua
+++ Config/Config.lua
@@ -1324,6 +1324,132 @@ local function NamePlatesOptions()
 							E:GetModule("NamePlates"):ConfigureAll()
 						end
 					},
+					guildGroup = {
+						order = 2,
+						type = "group",
+						name = L["Guild"],
+						guiInline = true,
+						get = function(info) return E.db.enhanced.nameplates.guild[info[#info]] end,
+						set = function(info, value)
+							E.db.enhanced.nameplates.guild[info[#info]] = value
+							E:GetModule("NamePlates"):ConfigureAll()
+						end,
+						disabled = function() return not E.db.enhanced.nameplates.titleCache end,
+						args = {
+							font = {
+								order = 1,
+								type = "select",
+								dialogControl = "LSM30_Font",
+								name = L["Font"],
+								values = AceGUIWidgetLSMlists.font,
+							},
+							fontSize = {
+								order = 2,
+								type = "range",
+								name = L["FONT_SIZE"],
+								min = 4, max = 33, step = 1,
+							},
+							fontOutline = {
+								order = 3,
+								type = "select",
+								name = L["Font Outline"],
+								values = {
+									["NONE"] = L["NONE"],
+									["OUTLINE"] = "OUTLINE",
+									["MONOCHROMEOUTLINE"] = "MONOCROMEOUTLINE",
+									["THICKOUTLINE"] = "THICKOUTLINE"
+								}
+							},
+							separator = {
+								order = 4,
+								type = "select",
+								name = L["Separator"],
+								values = {
+									[" "] = L["NONE"],
+									["<"] = "< >",
+									["("] = "( )",
+									["["] = "[ ]",
+									["{"] = "{ }"
+								}
+							},
+							colorsGroup = {
+								order = 5,
+								type = "group",
+								name = L["COLORS"],
+								guiInline = true,
+								get = function(info)
+									local t = E.db.enhanced.nameplates.guild.colors[info[#info]]
+									local d = P.enhanced.nameplates.guild.colors[info[#info]]
+									return t.r, t.g, t.b, t.a, d.r, d.g, d.b
+								end,
+								set = function(info, r, g, b)
+									local t = E.db.enhanced.nameplates.guild.colors[info[#info]]
+									t.r, t.g, t.b = r, g, b
+									E:GetModule("NamePlates"):ConfigureAll()
+								end,
+								args = {
+									raid = {
+										order = 1,
+										type = "color",
+										name = L["RAID"],
+									},
+									party = {
+										order = 2,
+										type = "color",
+										name = L["PARTY"],
+									},
+									guild = {
+										order = 3,
+										type = "color",
+										name = L["GUILD"],
+									},
+									none = {
+										order = 4,
+										type = "color",
+										name = L["ALL"],
+									},
+								}
+							},
+							visabilityGroup = {
+								order = 6,
+								type = "group",
+								name = L["Visibility State"],
+								guiInline = true,
+								get = function(info) return E.db.enhanced.nameplates.guild.visibility[info[#info]] end,
+								set = function(info, value)
+									E.db.enhanced.nameplates.guild.visibility[info[#info]] = value
+									E:GetModule("NamePlates"):ConfigureAll()
+								end,
+								args = {
+									city = {
+										order = 1,
+										type = "toggle",
+										name = L["City (Resting)"]
+									},
+									pvp = {
+										order = 2,
+										type = "toggle",
+										name = L["PvP"]
+									},
+									arena = {
+										order = 3,
+										type = "toggle",
+										name = L["Arena"]
+									},
+									party = {
+										order = 4,
+										type = "toggle",
+										name = L["Party"]
+									},
+									raid = {
+										order = 5,
+										type = "toggle",
+										name = L["Raid"]
+									}
+								}
+							}
+						}
+					},
 					npcGroup = {
 						order = 3,
 						type = "group",
